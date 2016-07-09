a491c69b7feb41702ef782f5166b6f0b0e216e2e
wisdom <zntpfls12@gmail.com>
2016-07-09 16:44:48 +0900

    객체리터럴 방식으로 코딩

--- js/main.js
+++ js/main.js
@@ -1,19 +1,37 @@
 $(function () {
    
-    var $tabMenu = $('.tab_menu li');
-    var $tabContents = $('.tab_contents li');
-    var currentIndex = 0;
-    var oldIndex = null;
-    
-    $tabMenu.on('click', function () {
-       oldIndex = currentIndex;
-        currentIndex = $(this).index();
-        
-        $tabMenu.removeClass('active');
-        $(this).addClass('active');
-        
-        $tabContents.eq(oldIndex).hide();
-        $tabContents.eq(currentIndex).show();
-    });
+    tabMenu.init();
     
 });
+
+
+var tabMenu = {
+    $tabMenu: null,
+    $tabContents:  null,
+    currentIndex : null,
+    oldIndex: null,
+    init: function () {
+        this.$tabMenu =  $('.tab_menu li');
+        this.$tabContents = $('.tab_contents li');
+        this.currentIndex = 0;
+        this.initEvent();
+    },
+    initEvent: function () {
+        var that = this;
+        this.$tabMenu.on('click', function () {
+            that.oldIndex = that.currentIndex;
+            that.currentIndex = $(this).index();
+
+            that.tabMenuOnOff($(this));
+            that.tabContentsOnOff();
+        });
+    },
+    tabMenuOnOff: function ($self) {
+        this.$tabMenu.removeClass('active');
+        $self.addClass('active');
+    },
+    tabContentsOnOff: function () {
+        this.$tabContents.eq(this.oldIndex).hide();
+        this.$tabContents.eq(this.currentIndex).show();
+    }
+};
